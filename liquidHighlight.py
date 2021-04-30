5036c350aaf99f11bf3d53eb5ba61b244bc272ee
Arkin Solomon <arkinsolomon@gmail.com>
2021-04-29 18:25:46 -0700

    Testing liquidHighlight.py (backing up)

--- liquidHighlight.py
+++ liquidHighlight.py
@@ -2,9 +2,19 @@
 # Replace markdown code highlights with liquid/rouge highlights
 
 # Imports
-from os import listdir
-from os.path import isfile, join
+from pathlib import Path
+import re
 
 # Get all files in _pages
 pagesPath = '_pages'
-onlyfiles = [f for f in listdir(pagesPath) if isfile(join(pagesPath, f))]
+files = list(Path(pagesPath).rglob('*.[mM][dD]'))#
+
+# Loop through all files
+for f in files:
+    file = open(f, 'w+')
+    text = file.read()
+    newText = re.sub(r'^```(\w+)?$', text, '{% highlight \1 linenos %}', flags=re.MULTILINE)
+    newText = re.sub(r'^```$', newText, '{% endhighlight %}', flags=re.MULTILINE)
+    file.seek(0)
+    file.write(newText)
+    file.close()
